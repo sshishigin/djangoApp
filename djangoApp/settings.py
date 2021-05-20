fa084926a44eacc097724f35dbf9ae0a9c148b84
Stepan <sshishigin190101@gmail.com>
2021-05-20 09:14:33 +0300

    Update settings.py

--- djangoApp/settings.py
+++ djangoApp/settings.py
@@ -150,12 +150,7 @@ STATICFILES_DIRS = [
 ]
 
 # Email backend
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sshishigin190101@gmail.com'
-EMAIL_HOST_PASSWORD = 'efomef6256A'
+#спрятал настройки
 
 # REST SETTINGS
 
