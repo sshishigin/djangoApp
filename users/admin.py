26103ddbba08fb433eef5bc7f93410f7594c6b3d
Stepan <shishigin190101@gmail.com>
2021-06-22 14:48:37 +0300

    Extended user admin page

--- users/admin.py
+++ users/admin.py
@@ -9,16 +9,18 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active',)
-    list_filter = ('email', 'is_staff', 'is_active',)
+    list_display = ('email', 'is_staff', 'is_active', 'id', 'first_name', 'last_name')
+    list_filter = ('email', 'is_staff', 'is_active', 'first_name', 'last_name')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Персональные данные', {'fields':('first_name', 'last_name')})
     )
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'first_name', 'last_name')}
         ),
     )
     search_fields = ('email',)
